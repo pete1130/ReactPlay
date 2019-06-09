21ca8ab3d8969eda42b4dd59701878d643f04f63
Peter Nguyen <petern1130@gmail.com>
2019-06-08 22:27:34 -0500

    reddit copy

--- reddit/src/Reddit.js
+++ reddit/src/Reddit.js
@@ -12,4 +12,55 @@ export default class Reddit extends Component {
             .then(json => this.processPosts(json.data.children))
             .catch(err => console.log(err));
     }
+
+    processPosts = posts => {
+        let postsHash = posts.reduce((hash, post) => {
+            hash[post.data.id] = post.data;
+            return hash;
+        }, {});
+
+        this.setState({
+            posts: postsHash
+        });
+    };
+
+    handleUpvote = postId => {
+        this.setState({
+            posts: {
+                ...this.state.posts,
+
+                [postId]: {
+                    ...this.state.posts[postId],
+                    score: this.state.posts[postId].score + 1
+                }
+            }
+        });
+    };
+
+    handleDownvote = postId => {
+        this.setState({
+            posts: {
+                ...this.state.posts,
+
+                [postId]: {
+                    ...this.state.posts[postId],
+                    score: this.state.posts[postId].score - 1
+                }
+            }
+        });
+    };
+
+    render() {
+        const posts = Object.keys(this.state.posts).map(
+            id => this.state.posts[id]
+        );
+
+        return (
+            <RedditListing
+                posts={posts}
+                onUpvote={this.handleUpvote}
+                onDownvote={this.handleDownvote}
+            />
+        );
+    }
 }
